ea578d9e690bf1373d85af210146821b559fd308
Gabriel Costa <ganovo.costa@outlook.com>
2026-02-16 14:45:57 -0300

    Feature: Timer

---
+++ src/App-GalaxyBook4.tsx
@@ -0,0 +1,18 @@
+import { Home } from "./pages/Home";
+
+import "./styles/theme.css";
+import "./styles/global.css";
+
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
+
+
+export function App() {
+
+    return (
+        <>
+            <TaskContextProvider >
+                <Home/>
+            </TaskContextProvider>
+        </>
+    )
+}; 
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react"
 import { TaskContext } from "./TaskContext"
 import { initialTaskState } from "./initialTaskState"
 import { taskReducer } from "./TaskReducer"
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager"
 
 type TaskContextProviderProps = {
     children: React.ReactNode
@@ -11,10 +12,19 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
     const [state, dispatch] = useReducer(taskReducer, initialTaskState)
 
+    const worker = TimerWorkerManager.getInstance()
+
+    worker.onmessage(e => {console.log(e.data)})
+
     useEffect(
         () => {
-            console.log(state);
-        }, [state]
+            if(!state.activeTask) {
+                console.log('Worer terminado por falta de task');
+                worker.terminate()
+            }
+            
+            worker.postMessage(state)
+        }, [state, worker]
     )
 
     return (
@@ -23,5 +33,7 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
         </TaskContext.Provider>
     )
 };
+
+
 
 
